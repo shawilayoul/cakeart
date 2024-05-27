1a86f96af0ca2f02c34ec8cbd1b9a76ce5bd55d4
Aochol <aocholayoul9@gmail.com>
2024-05-27 22:44:26 +0200

    working on responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,60 +2,70 @@ import { GrCart } from "react-icons/gr";
 import { FaSearch } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
 import { Link, useNavigate } from "react-router-dom";
-import "./header.scss";
 import { IoMenu } from "react-icons/io5";
 import { useContext, useState } from "react";
 import CakeContext from "../../contexts/CakeContexts";
+import "./header.scss";
 const Header = () => {
-  const [showBar,setShowBar] = useState(false)
+  const [showBar, setShowBar] = useState(false);
   const navigate = useNavigate();
-const{countProduct} = useContext(CakeContext)
+  const { countProduct } = useContext(CakeContext);
+  const menu = [
+    {
+      id: 1,
+      name: "home",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "Blog",
+      link: "blog",
+    },
+    {
+      id: 3,
+      name: "Recipes",
+      link: "recipes",
+    },
+    {
+      id: 4,
+      name: "shop",
+      link: "cart/shop",
+    },
+    {
+      id: 5,
+      name: "about",
+      link: "About",
+    },
+    {
+      id: 6,
+      name: "contact",
+      link: "Contact",
+    },
+  ];
   return (
     <nav className="headerContainer">
       <div className="appbar">
-        { showBar?  <IoClose onClick={()=>setShowBar(!showBar)}/>:
-            <IoMenu  onClick={()=>setShowBar(!showBar)}/> 
-          
-        }
-    
-      <div className="mobilBar">
-        {  showBar &&
-      <div className="menuLeft">
-          <ul>
-            <li>
-              <Link to="/" className="text-link">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="blog" className="text-link">
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link to="recipes" className="text-link">
-                Recipes
-              </Link>
-            </li>
-            <li>
-              <Link to="cart/shop" className="text-link">
-                Shop
-              </Link>
-            </li>
-            <li>
-              <Link to="about" className="text-link">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to="contact" className="text-link">
-                Contact
-              </Link>
-            </li>
-          </ul>
+        {showBar ? (
+          <IoClose onClick={() => setShowBar(!showBar)} />
+        ) : (
+          <IoMenu onClick={() => setShowBar(!showBar)} />
+        )}
+
+        <div className="mobilBar">
+          {showBar && (
+            <div className="menuLeft">
+              <ul>
+                {menu.map(({ id, name, link }) => (
+                  <li key={id} onClick={() => setShowBar(!showBar)}>
+                    <Link to={link} className="text-link">
+                      {name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-        }
-      </div>
       </div>
       <div className="logo">
         <h2 onClick={() => navigate("/")}>
@@ -65,36 +75,13 @@ const{countProduct} = useContext(CakeContext)
       <div className="menu">
         <div className="menuLeft">
           <ul>
-            <li>
-              <Link to="/" className="text-link">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="blog" className="text-link">
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link to="recipes" className="text-link">
-                Recipes
-              </Link>
-            </li>
-            <li>
-              <Link to="cart/shop" className="text-link">
-                Shop
-              </Link>
-            </li>
-            <li>
-              <Link to="about" className="text-link">
-                About 
-              </Link>
-            </li>
-            <li>
-              <Link to="contact" className="text-link">
-                Contact
-              </Link>
-            </li>
+            {menu.map(({ id, name, link }) => (
+              <li onClick={() => setShowBar(!showBar)}>
+                <Link to={link} className="text-link" key={id}>
+                  {name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="menuRight">
